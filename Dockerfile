76d6d39040602793d91951d5625085010d248d24
Murray Oldfield <Murray.Oldfield@intersystems.com>
2024-05-23 10:31:19 +1000

    Update Dockerfile and improve GUI of application
    The Dockerfile was updated by adding Python supporting packages and setting the DISPLAY environment variable. Also, the graphical user interface for the 'YaspeApp' was significantly improved, by modifying existing labels for better clarity and adding short descriptions for fields to guide the user's input. Some changes were made in requirements.txt file too, for adding supporting libraries.

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,11 @@ RUN apt-get update && \
 	sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && \
     dpkg-reconfigure --frontend=noninteractive locales
     
+RUN apt-get update && apt-get install -y \
+    python3-tk libgl1-mesa-glx \
+    && rm -rf /var/lib/apt/lists/*
+
+    
 ENV LC_ALL en_US.UTF-8 
 ENV LANG en_US.UTF-8  
 ENV LANGUAGE en_US:en   
@@ -17,3 +22,6 @@ RUN pip3 install --upgrade pip
 RUN pip3 install -r requirements.txt --default-timeout=100 --no-cache-dir
 
 COPY . .
+
+# Set the display environment variable
+ENV DISPLAY=:0
--- yaspe_runner.py
+++ yaspe_runner.py
@@ -9,42 +9,44 @@ import threading
 class YaspeApp:
     def __init__(self, root):
         self.root = root
-        self.root.title("YASPE Argument Prompter")
+        self.root.title("YASPE")
 
         padx = 10  # Padding for the left and right side
-        pady = 5  # Padding for the top and bottom
+        pady = 10  # Padding for the top and bottom
 
         # Input Files
-        tk.Label(root, text="Input HTML Files:").grid(row=0, column=0, sticky="e", padx=(padx, 0), pady=pady)
+        tk.Label(root, text="Input HTML Files. You can select more than one:").grid(
+            row=0, column=0, sticky="e", padx=(padx, 0), pady=pady
+        )
         self.input_files = tk.Entry(root, width=50)
         self.input_files.grid(row=0, column=1, pady=pady, padx=(0, padx))
         tk.Button(root, text="Browse", command=self.browse_input_files).grid(row=0, column=2, pady=pady, padx=(0, padx))
 
-        # Iostat Checkbox
-        self.var_iostat = tk.BooleanVar()
-        tk.Checkbutton(root, text="Plot Iostat Data", variable=self.var_iostat).grid(
+        # System Overview Checkbox
+        self.var_system = tk.BooleanVar(value=True)
+        tk.Checkbutton(root, text="Output System Overview. Very useful.", variable=self.var_system).grid(
             row=1, column=1, sticky="w", pady=pady, padx=(padx, 0)
-        )
-
-        # Nfsiostat Checkbox
-        self.var_nfsiostat = tk.BooleanVar()
-        tk.Checkbutton(root, text="Plot Nfsiostat Data", variable=self.var_nfsiostat).grid(
-            row=2, column=1, sticky="w", pady=pady, padx=(padx, 0)
         )
 
         # Append Checkbox (defaulted to unchecked)
         self.var_append = tk.BooleanVar(value=False)
-        tk.Checkbutton(root, text="Append to Existing Database", variable=self.var_append).grid(
-            row=3, column=1, sticky="w", pady=pady, padx=(padx, 0)
-        )
+        tk.Checkbutton(
+            root,
+            text="Append to Existing Database. Select multiple HTML files and use an 'output file prefix' (below).",
+            variable=self.var_append,
+        ).grid(row=3, column=1, sticky="w", pady=pady, padx=(padx, 0))
 
         # Output Prefix
-        tk.Label(root, text="Output File Prefix:").grid(row=4, column=0, sticky="e", pady=pady, padx=(padx, 0))
+        tk.Label(root, text="Output File Prefix. HTML file name used by default:").grid(
+            row=4, column=0, sticky="e", pady=pady, padx=(padx, 0)
+        )
         self.output_prefix = tk.Entry(root, width=50)
         self.output_prefix.grid(row=4, column=1, pady=pady, padx=(0, padx))
 
         # Existing Database
-        tk.Label(root, text="Existing Database File:").grid(row=5, column=0, sticky="e", pady=pady, padx=(padx, 0))
+        tk.Label(
+            root, text="Existing SQLite Database File. If 'Append' used on previous run. Uncheck 'Append' now:"
+        ).grid(row=5, column=0, sticky="e", pady=pady, padx=(padx, 0))
         self.existing_database = tk.Entry(root, width=50)
         self.existing_database.grid(row=5, column=1, pady=pady, padx=(0, padx))
         tk.Button(root, text="Browse", command=self.browse_existing_database).grid(
@@ -52,52 +54,66 @@ class YaspeApp:
         )
 
         # CSV Checkbox
-        self.var_csv = tk.BooleanVar()
-        tk.Checkbutton(root, text="Create CSV Files", variable=self.var_csv).grid(
-            row=6, column=1, sticky="w", pady=pady, padx=(padx, 0)
-        )
-
-        # PNG Checkbox
-        self.var_png = tk.BooleanVar()
-        tk.Checkbutton(root, text="Create PNG Files", variable=self.var_png).grid(
+        self.var_csv = tk.BooleanVar(value=True)
+        tk.Checkbutton(root, text="Create CSV Files. So you can make your own plots.", variable=self.var_csv).grid(
             row=7, column=1, sticky="w", pady=pady, padx=(padx, 0)
         )
 
-        # System Overview Checkbox
-        self.var_system = tk.BooleanVar()
-        tk.Checkbutton(root, text="Output System Overview", variable=self.var_system).grid(
-            row=8, column=1, sticky="w", pady=pady, padx=(padx, 0)
-        )
+        # PNG Checkbox
+        self.var_png = tk.BooleanVar(value=True)
+        tk.Checkbutton(
+            root,
+            text="Create PNG Files. Instead of HTML plots (will take a little longer). Good for reports.",
+            variable=self.var_png,
+        ).grid(row=8, column=1, sticky="w", pady=pady, padx=(padx, 0))
 
-        # Date Format Checkbox
-        self.var_ddmmyyyy = tk.BooleanVar()
-        tk.Checkbutton(root, text="Date Format DDMMYYYY", variable=self.var_ddmmyyyy).grid(
-            row=9, column=1, sticky="w", pady=pady, padx=(padx, 0)
+        # Iostat Checkbox
+        self.var_iostat = tk.BooleanVar()
+        tk.Checkbutton(root, text="Plot iostat data. This can take a long time.", variable=self.var_iostat).grid(
+            row=10, column=1, sticky="w", pady=pady, padx=(padx, 0)
         )
 
         # Disk List
-        tk.Label(root, text="Disk List:").grid(row=10, column=0, sticky="e", pady=pady, padx=(padx, 0))
+        tk.Label(root, text="Disk List. For example limit to only dm-0 dm-3 sdf:").grid(
+            row=12, column=0, sticky="e", pady=pady, padx=(padx, 0)
+        )
         self.disk_list = tk.Entry(root, width=50)
-        self.disk_list.grid(row=10, column=1, pady=pady, padx=(0, padx))
+        self.disk_list.grid(row=12, column=1, pady=pady, padx=(0, padx))
+
+        # Nfsiostat Checkbox
+        self.var_nfsiostat = tk.BooleanVar()
+        tk.Checkbutton(
+            root,
+            text="Plot nfsiostat data. Not included by default in ^SystemPerformance.",
+            variable=self.var_nfsiostat,
+        ).grid(row=13, column=1, sticky="w", pady=pady, padx=(padx, 0))
 
         # Large File Split String
-        tk.Label(root, text="String to Split On:").grid(row=11, column=0, sticky="e", pady=pady, padx=(padx, 0))
+        tk.Label(root, text="String to split on. If '' will split at iostat:").grid(
+            row=14, column=0, sticky="e", pady=pady, padx=(padx, 0)
+        )
         self.large_file_split_on_string = tk.Entry(root, width=50)
-        self.large_file_split_on_string.grid(row=11, column=1, pady=pady, padx=(0, padx))
+        self.large_file_split_on_string.grid(row=14, column=1, pady=pady, padx=(0, padx))
+
+        # Date Format Checkbox
+        self.var_ddmmyyyy = tk.BooleanVar()
+        tk.Checkbutton(root, text="Date Format DDMMYYYY for csv files.", variable=self.var_ddmmyyyy).grid(
+            row=15, column=1, sticky="w", pady=pady, padx=(padx, 0)
+        )
 
         # Version Checkbox
         self.var_version = tk.BooleanVar()
         tk.Checkbutton(root, text="Show Version", variable=self.var_version).grid(
-            row=12, column=1, sticky="w", pady=pady, padx=(padx, 0)
+            row=17, column=1, sticky="w", pady=pady, padx=(padx, 0)
         )
 
         # Submit Button
         tk.Button(root, text="Submit", command=self.submit).grid(
-            row=13, column=1, pady=pady, padx=(0, padx), sticky="e"
+            row=19, column=1, pady=pady, padx=(0, padx), sticky="e"
         )
 
         # Exit Button
-        tk.Button(root, text="Exit", command=root.quit).grid(row=13, column=2, pady=pady, padx=(0, padx), sticky="w")
+        tk.Button(root, text="Exit", command=root.quit).grid(row=19, column=2, pady=pady, padx=(0, padx), sticky="w")
 
         # Ensure the window gains focus
         self.root.focus_force()
@@ -119,7 +135,7 @@ class YaspeApp:
         existing_database = self.existing_database.get()
 
         if not input_files and not existing_database:
-            messagebox.showerror("Error", "Please provide at least one input file or an existing database.")
+            messagebox.showerror("Error", "Please provide at least one input HTML file or an existing SQLite database.")
             return
 
         wait_window = Toplevel(self.root)
@@ -164,10 +180,10 @@ class YaspeApp:
             try:
                 subprocess.run(args, check=True)
             except subprocess.CalledProcessError as e:
-                self.root.after(0, messagebox.showerror, "Error", f"Error executing YASPE for target {target}: {e}")
+                self.root.after(0, messagebox.showerror, "Error", f"Error executing YASPE for HTML {target}: {e}")
 
         self.root.after(0, wait_window.destroy)
-        self.root.after(0, messagebox.showinfo, "Success", "YASPE executed successfully for all targets.")
+        self.root.after(0, messagebox.showinfo, "Success", "YASPE executed successfully for all files.")
 
 
 if __name__ == "__main__":
