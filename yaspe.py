b9da8863bd3288ad6d023f34d78fa2fa9880bd71
Murray Oldfield <Murray.Oldfield@intersystems.com>
2026-09-16 17:27:27 +1000

    Bump version: 0.14.2 → 0.14.3

--- yaspe.py
+++ yaspe.py
@@ -3868,7 +3868,7 @@ if __name__ == "__main__":
         prog="yaspe", description="Performance file review.", epilog='Be safe, "quote the path"'
     )
 
-    current_version = "0.14.2"
+    current_version = "0.14.3"
     parser.add_argument("-v", "--version", action="version", version=current_version)
 
     parser.add_argument(
